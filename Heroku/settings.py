6ab5a2acff48d6d09dcda88ab05fec00dcb8ffed
sktkddn777 <37575974+sktkddn777@users.noreply.github.com>
2021-06-05 02:10:23 +0900

    using PostgreSQL in Django

--- Heroku/settings.py
+++ Heroku/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'Heroku.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django_test',
+        'USER': 'postgres',
+        'PASSWORD': '1357',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
